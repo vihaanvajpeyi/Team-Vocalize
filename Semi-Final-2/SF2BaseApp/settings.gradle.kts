e97cc46134325c92f5488e361eab24a2f6a93ded
vihaanvajpeyi <m875c7ynvj@privaterelay.appleid.com>
2025-12-04 11:57:35 +0530

    Create settings.gradle.kts

---
+++ Semi-Final-2/SF2BaseApp/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google {}
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_PROJECT)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Vocalize SF2"
+include(":app")
+ 
